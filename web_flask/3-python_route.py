27740b38507350a2dd5eeffab8a44455f5a0831e
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-01-16 14:40:35 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
 from flask import Flask
-"""
-intializing flask web app to listen on 0.0.0.0:5000
-"""
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
-    """
-    Display "Hello HBNB!"
-    """
-    return ('Hello HBNB!')
+def hello_hbnb():
+    """ Print Web """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """
-    route /hbnb displays
-    """
-    return ('HBNB')
+    """ Print Web """
+    return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c(text):
-    """
-    replaces _ with spaces
-    """
-    return ("C {}".format(text.replace('_', ' ')))
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
-def py(text="is cool"):
-    """
-    default adds is cool and replaces _ with space
-    """
-    return("Python {}".format(text.replace('_', ' ')))
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool"""
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
