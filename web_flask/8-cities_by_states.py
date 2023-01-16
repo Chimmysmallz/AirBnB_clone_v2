e15b8301ac966f04c2a129fe553ad1ab08a4e55d
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-01-16 14:44:32 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
 """
-intializing flask web app to listen on 0.0.0.0:5000
-"""
 from flask import Flask, render_template
-from models import storage, classes
+from models import storage
+from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
-
-
-@app.route('/cities_by_states')
-def cities_by_states():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    """
+def closedb(exc):
+    """ to close a database session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/cities_by_states')
+def states_list():
+    """ /states_list route """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
