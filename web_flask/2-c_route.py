df58b33ce43fb3138b1362580838ba229ccee8d2
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-01-16 14:33:21 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
+"""script that starts a Flask web application"""
 from flask import Flask
-"""
-intializing flask web app to listen on 0.0.0.0:5000
-"""
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
-    """
-    Display "Hello HBNB!"
-    """
-    return ('Hello HBNB!')
+def hello_hbnb():
+    """print web"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """
-    route /hbnb displays
-    """
-    return ('HBNB')
+    """print web"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c(text):
-    """
-    replaces _ with spaces
-    """
-    return ("C {}".format(text.replace('_', ' ')))
+def c_is_fun(text):
+    """print C followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
